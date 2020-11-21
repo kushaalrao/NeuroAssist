391272574e28e38bc75f22f1a8a329755ecf2a33
Priyanshi Nigam <priyanshi.nigam@gmail.com>
2020-11-21 13:22:31 -0800

    fix bugs

--- bci_classifier.py
+++ bci_classifier.py
@@ -14,7 +14,7 @@ BUFFER_LENGTH   = 5
 EPOCH_LENGTH    = 1
 OVERLAP_LENGTH  = 0.8
 SHIFT_LENGTH    = EPOCH_LENGTH - OVERLAP_LENGTH
-TRAINING_LENGTH = 20
+TRAINING_LENGTH = 2
 
 
 if __name__ == "__main__":
@@ -22,70 +22,71 @@ if __name__ == "__main__":
     # Parse Args
     parser = argparse.ArgumentParser(description='BCI Workshop example 2')
     parser.add_argument('channels', metavar='N', type=int, nargs='*',
-        default =[0, 1, 2, 3],
-        help ='channel number to use. If not specified, all the channels are used')
+    default =[0, 1, 2, 3],
+    help ='channel number to use. If not specified, all the channels are used')
 
-    args          = parser.parse_args()
-    channel_index = args.channels
+    args = parser.parse_args()
+    channel_index = args.channels # ???
 
     # Connect to Muse
     print("Connecting...")
-    streams             = resolve_byprop('type', 'EEG', timeout = 2)
-    inlet               = StreamInlet(streams[0], max_chunklen=12)
-	eeg_time_correction = inlet.time_correction()
+    streams = resolve_byprop('type', 'EEG', timeout = 2)
+    inlet = StreamInlet(streams[0], max_chunklen=12)
+    eeg_time_correction = inlet.time_correction()
 
-	# Pull relevant information form stream
-	info       = inlet.info()
-	desc       = info.desc()
-	freq       = int(info.nominal_srate())
-	n_channels = info.channel_count()
+    # Pull relevant information form stream
+    info = inlet.info()
+    desc = info.desc()
+    freq = int(info.nominal_srate())
+    n_channels = info.channel_count()
 
-	# Record data to train classifier
-	print('Recording Mental Activity 0')
-	eeg_data0 = BCI.record_eeg(TRAINING_LENGTH, freq, channel_index)
+    # Record data to train classifier
+    print('Recording Mental Activity 0')
+    eeg_data0 = BCI.record_eeg(TRAINING_LENGTH, freq, channel_index)
     print('Recording Mental Activity 1')
-	eeg_data1 = BCI.record_eeg(TRAINING_LENGTH, freq, channel_index)
-	print('Recording Mental Activity 2')
-	eeg_data2 = BCI.record_eeg(TRAINING_LENGTH, freq, channel_index)
-	print('Recording Mental Activity 3')
-	eeg_data3 = BCI.record_eeg(TRAINING_LENGTH, freq, channel_index)
+    eeg_data1 = BCI.record_eeg(TRAINING_LENGTH, freq, channel_index)
+    print('Recording Mental Activity 2')
+    eeg_data2 = BCI.record_eeg(TRAINING_LENGTH, freq, channel_index)
+    print('Recording Mental Activity 3')
+    eeg_data3 = BCI.record_eeg(TRAINING_LENGTH, freq, channel_index)
 
 
-	# Divide data into epochs
-	epochs_0 = BCI.epoch(eeg_data0, EPOCH_LENGTH * freq,
-							OVERLAP_LENGTH * freq)
-	epochs_1 = BCI.epoch(eeg_data1, EPOCH_LENGTH * freq,
-							OVERLAP_LENGTH * freq)
-	epochs_2 = BCI.epoch(eeg_data2, EPOCH_LENGTH * freq,
-							OVERLAP_LENGTH * freq)
-	epochs_3 = BCI.epoch(eeg_data3, EPOCH_LENGTH * freq,
-							OVERLAP_LENGTH * freq)
+    # Divide data into epochs
+    epochs_0 = BCI.epoch_array(eeg_data0, EPOCH_LENGTH,
+    OVERLAP_LENGTH * freq, freq)
+    epochs_1 = BCI.epoch_array(eeg_data1, EPOCH_LENGTH,
+    OVERLAP_LENGTH * freq, freq)
+    epochs_2 = BCI.epoch_array(eeg_data2, EPOCH_LENGTH,
+    OVERLAP_LENGTH * freq, freq)
+    epochs_3 = BCI.epoch_array(eeg_data3, EPOCH_LENGTH,
+    OVERLAP_LENGTH * freq, freq)
 
-	# Compute corresponding feature matrices
-	feat_matrix0 = BCI.compute_feature_matrix(epochs_0, freq)
+    # Compute corresponding feature matrices
+    feat_matrix0 = BCI.compute_feature_matrix(epochs_0, freq)
     feat_matrix1 = BCI.compute_feature_matrix(epochs_1, freq)
     feat_matrix2 = BCI.compute_feature_matrix(epochs_2, freq)
     feat_matrix3 = BCI.compute_feature_matrix(epochs_3, freq)
 
     # Train Classifier
-    [classifier, mu_ft, std_ft, score] = BCIw.train_classifier(
-            feat_matrix0, feat_matrix1,
-            feat_matrix2, feat_matrix3,
-            feat_matrix4, 'RandomForestClassifier')
+    [classifier, mu_ft, std_ft, score] = BCI.train_classifier(
+    feat_matrix0, feat_matrix1,
+    feat_matrix2, feat_matrix3,'RandomForestClassifier')
 
 
     print(str(score * 100) + '% correctly predicted')
 
     # Initialize buffers for real time data
     eeg_buffer       = np.zeros((int(freq * BUFFER_LENGTH), n_channels))
+    print("eeg buffer")
+    print(eeg_buffer.shape)
     filter_state     = None
     decision_buffer  = np.zeros((30, 1))
 
-	# Plotter
-	plotter_decision = BCIw.DataPlotter(30, ['Decision'])
+    # Plotter
+    plotter_decision = BCI.DataPlotter(30, ['Decision'])
 
-	# Initialize socket instance
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Initialize socket instance
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Create a port for the socket
     s.bind((socket.gethostname(), 1456))
     # Listen for an avaliable client for 5 seconds
@@ -93,15 +94,17 @@ if __name__ == "__main__":
 
     # Start acquiring data and computing features
     try:
-    	while True:
+        while True:
 
-    		# Pull data
-    		eeg_data = BCI.record_eeg(SHIFT_LENGTH, freq, channel_index)
+            # Pull data
+            eeg_data = BCI.record_eeg(SHIFT_LENGTH, freq, channel_index)
 
-    		# Update EEG buffer
+            ch_data = np.array(eeg_data)[:, channel_index]
+
+            # Update EEG buffer
             eeg_buffer, filter_state = BCI.update_buffer(
-                    eeg_buffer, ch_data, notch = True,
-                    filter_state = filter_state)
+            eeg_buffer, ch_data, apply_filter = True,
+            filter_state = filter_state)
 
             # Get latest epoch
             epoch = BCI.get_last_data(eeg_buffer, EPOCH_LENGTH * freq)
@@ -111,27 +114,27 @@ if __name__ == "__main__":
 
             # Use band powers to test classifier
             y_hat = BCI.test_classifier(classifier, band_powers.reshape(1, -1),
-            							 mu_ft, std_ft)
+            mu_ft, std_ft)
 
             # Update decision buffer
-            decision_buffer, _ = BCIw.update_buffer(decision_buffer,
-                                                    np.reshape(y_hat, (-1, 1)))
+            decision_buffer, _ = BCI.update_buffer(decision_buffer,
+            np.reshape(y_hat, (-1, 1)))
 
             # accept client and get client IP
             clientsocket, address = s.accept()
             print("Connection has been established.")
             pred = np.mean(decision_buffer)
-			# input for Rasberry Pi
-			x = str(pred)
+            # input for Raspberry Pi
+            x = str(pred)
             print(x)
 
-			# sends data
-			clientsocket.send(bytes(x, 'utf-8'))
+            # sends data
+            clientsocket.send(bytes(x, 'utf-8'))
 
-			# Visualize decisions
-			plotter_decision.update_plot(decision_buffer)
+            # Visualize decisions
+            plotter_decision.update_plot(decision_buffer)
             plt.pause(0.00001)
 
     except KeyboardInterrupt:
 
-    	 print('Closing program')
+            print('Closing program')
--- bci_helper.py
+++ bci_helper.py
@@ -6,112 +6,120 @@ import sys
 from tempfile import gettempdir
 from subprocess import call
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from scipy.signal import butter, lfilter, lfilter_zi
+from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
+
 
 # initialize notches for bandpass filter
 NOTCH_B, NOTCH_A = butter(4, np.array([55,65])/(256/2), btype = 'bandstop')
 
 def record_eeg(r_length, freq, channel_i):
+	streams = resolve_byprop('type', 'EEG', timeout=2)
+
+	inlet  = StreamInlet(streams[0], max_chunklen=12)
 	""" Records EEG data from headset
 
 	Arguments:
 
-		r_length  -- how seconds of data to record
-		freq      -- sample rate
-		channel_i -- channels to keep data from
+	r_length  -- how many seconds of data to record
+	freq      -- sample rate
+	channel_i -- channels to keep data from
 
 	Returns:
 
-		data      -- array of recorded data [sample, channel]
+	data      -- array of recorded data [sample, channel]
 
 	"""
 
 	data, timestamps = inlet.pull_chunk(
-            timeout = r_length + 1, max_samples = freq * r_length)
-    
-    data = np.array(data)[:, channel_i]
+	timeout = r_length + 1, max_samples = int(freq * r_length))
 
-    return data
+	data = np.array(data)[:, channel_i]
+
+	return data
 
 
 def update_buffer(buffer, new_data, apply_filter = False, filter_state = None):
-   	""" Title: BCI Workshop Auxiliary Tools
-        Author: Cassani
-        Date: May 08 2015
-        Availability: https://github.com/NeuroTechX/bci-workshop """
+	""" Title: BCI Workshop Auxiliary Tools
+	Author: Cassani
+	Date: May 08 2015
+	Availability: https://github.com/NeuroTechX/bci-workshop """
 
-    """ Updates buffer with 'new_data' and can apply butterworth filter
+	""" Updates buffer with 'new_data' and can apply butterworth filter
 
-   	Arguments:
-    
-    	buffer       -- array for eeg data buffer [samples][channels]
-   		new_data     -- array with new data from channel [samples][channels]
-   		apply_filter -- (Boolean) when True, apply filter to buffer
-   		filter_state -- array of filtered values
+	Arguments:
 
-   	Returns:
+	buffer       -- array for eeg data buffer [samples][channels]
+	new_data     -- array with new data from channel [samples][channels]
+	apply_filter -- (Boolean) when True, apply filter to buffer
+	filter_state -- array of filtered values
 
-   		new_buffer   -- array of updated buffer [samples][channels]
+	Returns:
 
-    """
-    if new_data.ndim == 1:
-        new_data = new_data.reshape(-1, data_buffer.shape[1])
+	new_buffer   -- array of updated buffer [samples][channels]
 
-    if apply_filter:
-        if filter_state is None:
- 			# initialize filter state on buffer array
-            filter_state = np.tile(lfilter_zi(NOTCH_B, NOTCH_A),
-                                   (data_buffer.shape[1], 1)).T
-        
-        new_data, filter_state = (NOTCH_B, NOTCH_A, new_data, axis=0,
-                                         zi=filter_state)
+	"""
+	if new_data.ndim == 1:
+		new_data = new_data.reshape(-1, buffer.shape[1])
 
-    new_buffer = np.concatenate((data_buffer, new_data), axis=0)
-    new_buffer = new_buffer[new_data.shape[0]:, :]
+	if apply_filter:
+		if filter_state is None:
+			# initialize filter state on buffer array
+			filter_state = np.tile(lfilter_zi(NOTCH_B, NOTCH_A),
+			(buffer.shape[1], 1)).T
+			print(filter_state)
+			print(buffer.shape[1], 1)
 
-    return new_buffer, filter_state
+		new_data, filter_state = lfilter(NOTCH_B, NOTCH_A, new_data, axis=0,
+			zi=filter_state)
+
+	new_buffer = np.concatenate((buffer, new_data), axis=0)
+	new_buffer = new_buffer[new_data.shape[0]:, :]
+
+	return new_buffer, filter_state
 
 
 
 def epoch_array(eeg_data, epoch_length, overlap_length, freq):
 	""" Title: BCI Workshop Auxiliary Tools
-        Author: Cassani
-        Date: May 08 2015
-        Availability: https://github.com/NeuroTechX/bci-workshop """
+	Author: Cassani
+	Date: May 08 2015
+	Availability: https://github.com/NeuroTechX/bci-workshop """
 
-    """ Divides eeg data into epochs of specified length
+	""" Divides eeg data into epochs of specified length
 
-	Arguments: 
+	Arguments:
 
-		eeg_data  -- 2D array of eeg_data [samples, channels]
-		n_samples -- number of samples for each epoch (int)
-		overlap   -- number of samples that overlap from previous epoch
-		freq      -- sample rate
+	eeg_data  -- 2D array of eeg_data [samples, channels]
+	n_samples -- number of samples for each epoch (int)
+	overlap   -- number of samples that overlap from previous epoch
+	freq      -- sample rate
 
 	Returns:
 
-		epochs    -- 3D array of epochs [epoch_samples, channels, epochs]
-	
-    
+	epochs    -- 3D array of epochs [epoch_samples, channels, epochs]
+
+
 
 	"""
+	freq = np.abs(freq)
 
 	n_total_samples, n_channels = eeg_data.shape
-	
+
 	# convert seconds to number of samples
 	epoch_n_samples   = epoch_length * freq
-	overlap_n_samples = overlap_length * freq
-	shift_n_sammples  = epoch_n_samples - overlap_n_samples
+	overlap_n_samples = 0#overlap_length * freq
+	shift_n_samples  = epoch_n_samples - overlap_n_samples
 
 	# total number of epochs
-	n_epochs = int(np.floor((n_total_samples - epoch_n_samples) 
-		/ float(shift_n_sammples)) + 1)
+	n_epochs = int(np.floor((n_total_samples - epoch_n_samples)
+	/ float(shift_n_samples)) + 1)
 
 	# markers to indicate where epochs start and how many samples per epoch
-	markers = np.asarray(range(0, n_epochs + 1)) * shift_n_sammples
+	markers = np.asarray(range(0, n_epochs + 1)) * shift_n_samples
 	markers = markers.astype(int)
 
 	# initialize array to hold epoch data
@@ -121,30 +129,30 @@ def epoch_array(eeg_data, epoch_length, overlap_length, freq):
 	for i in range(0, n_epochs):
 		epochs[:,:,i] = eeg_data[markers[i]:markers[i] + epoch_n_samples, :]
 
-	return epochs
+		return epochs
 
 
 def get_last_data(eeg_buffer, n_samples):
-    """ Title: BCI Workshop Auxiliary Tools
-        Author: Cassani
-        Date: May 08 2015
-        Availability: https://github.com/NeuroTechX/bci-workshop """
-    """ Grabs latest data from buffer
+	""" Title: BCI Workshop Auxiliary Tools
+	Author: Cassani
+	Date: May 08 2015
+	Availability: https://github.com/NeuroTechX/bci-workshop """
+	""" Grabs latest data from buffer
 
-    Arguments:
+	Arguments:
 
-    	eeg_buffer -- array of raw eeg data [samples][channels]
-    	n_samples  -- number of samples to to pull from buffer
-    
-    Returns:
+	eeg_buffer -- array of raw eeg data [samples][channels]
+	n_samples  -- number of samples to to pull from buffer
 
-    	last_data -- array that holds latest data [values]
+	Returns:
 
-    """
+	last_data -- array that holds latest data [values]
 
-    last_data = eeg_buffer[(eeg_buffer.shape[0] - n_samples):, :]
+	"""
 
-    return last_data
+	last_data = eeg_buffer[(eeg_buffer.shape[0] - n_samples):, :]
+
+	return last_data
 
 
 def next_pow_2(x):
@@ -152,12 +160,12 @@ def next_pow_2(x):
 
 	Arguments:
 
-		x -- input value
+	x -- input value
 
 	Returns:
 
-		y -- next power of 2
-	
+	y -- next power of 2
+
 	"""
 
 	n = 1
@@ -169,182 +177,175 @@ def next_pow_2(x):
 
 def compute_band_powers(epoch, freq):
 	""" Title: BCI Workshop Auxiliary Tools
-        Author: Cassani
-        Date: May 08 2015
-        Availability: https://github.com/NeuroTechX/bci-workshop """
+	Author: Cassani
+	Date: May 08 2015
+	Availability: https://github.com/NeuroTechX/bci-workshop """
 
-    """ Computes band powers as feature matrix from epoch
+	""" Computes band powers as feature matrix from epoch
 
-	Arguments: 
-	
-		epoch       -- 2D array of epoch data [samples, channels]
-		freq        -- (float) sample rate
+	Arguments:
+
+	epoch       -- 2D array of epoch data [samples, channels]
+	freq        -- (float) sample rate
 
 	Returns:
-	
-		band_powers --  2D array containing features 
-			        --  [feature values, different features (delta, theta etc...)]
+
+	band_powers --  2D array containing features
+	--  [feature values, different features (delta, theta etc...)]
 	"""
 
 	sample_length, n_channels = epoch.shape
 
-	# Hammming Window 
-    hwin            = np.hamming(sample_length)
-    data_centered   = epoch - np.mean(epoch, axis=0)
-    h_data_centered = (data_centered.T * hwin).T
+	# Hammming Window
+	hwin            = np.hamming(sample_length)
+	data_centered   = epoch - np.mean(epoch, axis=0)
+	h_data_centered = (data_centered.T * hwin).T
 
-    # length of axis for FFT output 
-    n_axis = nextpow2(sample_length)
-   	
-   	# compute one-dimensional FFT 
-    fastFT = np.fft.fft(h_data_centered, n = n_axis, axis = 0) / sample_length
-    
-    # compute Power Spectral Density (true theoretical FFT)
-    PSD = 2 * np.abs(fastFT[0:int(n_axis / 2), :])
-    
-    # frequency bins so separate features
-    freq_bin = freq / 2 * np.linspace(0, 1, int(n_axis / 2))
+	# length of axis for FFT output
+	n_axis = next_pow_2(sample_length)
+
+	# compute one-dimensional FFT
+	fastFT = np.fft.fft(h_data_centered, n = n_axis, axis = 0) / sample_length
+
+	# compute Power Spectral Density (true theoretical FFT)
+	PSD = 2 * np.abs(fastFT[0:int(n_axis / 2), :])
+
+	# frequency bins so separate features
+	freq_bin = freq / 2 * np.linspace(0, 1, int(n_axis / 2))
 
 
-    # Seperate band powers and take mean for the epoch
-    # Delta < 4
-    delta_index, = np.where(freq_bin < 4)
-    meanDelta    = np.mean(PSD[delta_index, :], axis=0)
-    
-    # Theta 4-8
-    theta_index, = np.where((freq_bin >= 4) & (freq_bin <= 8))
-    meanTheta    = np.mean(PSD[theta_index, :], axis=0)
-    
-    # Alpha 8-12
-    alpha_index, = np.where((freq_bin >= 8) & (freq_bin <= 12))
-    meanAlpha    = np.mean(PSD[alpha_index, :], axis=0)
-    
-    # Beta 12-30
-    beta_index,  = np.where((freq_bin >= 12) & (freq_bin < 30))
-    meanBeta     = np.mean(PSD[beta_index, :], axis=0)
+	# Separate band powers and take mean for the epoch
+	# Delta < 4
+	delta_index, = np.where(freq_bin < 4)
+	meanDelta    = np.mean(PSD[delta_index, :], axis=0)
 
-    band_powers  = np.concatenate((meanDelta, meanTheta, 
-    										meanAlpha, meanBeta), axis=0)
-   	band_powers  = np.log10(band_powers)
+	# Theta 4-8
+	theta_index, = np.where((freq_bin >= 4) & (freq_bin <= 8))
+	meanTheta    = np.mean(PSD[theta_index, :], axis=0)
 
-    return band_powers
+	# Alpha 8-12
+	alpha_index, = np.where((freq_bin >= 8) & (freq_bin <= 12))
+	meanAlpha    = np.mean(PSD[alpha_index, :], axis=0)
+
+	# Beta 12-30
+	beta_index,  = np.where((freq_bin >= 12) & (freq_bin < 30))
+	meanBeta     = np.mean(PSD[beta_index, :], axis=0)
+
+	band_powers  = np.concatenate((meanDelta, meanTheta,
+	meanAlpha, meanBeta), axis=0)
+	band_powers  = np.log10(band_powers)
+
+	return band_powers
 
 
 def compute_feature_matrix(epochs, freq):
-    """ Title: BCI Workshop Auxiliary Tools
-        Author: Cassani
-        Date: May 08 2015
-        Availability: https://github.com/NeuroTechX/bci-workshop """
-    """ Computes band powers for multiple epochs
-
-    Arguments:
-
-    	epochs         -- 3D array of epochs [epoch_samples, channels, epochs]
-    	freq           -- (float)sample rate
-
-    Returns:
-
-    	feature_matrix -- matrix of epochs and corresponding features
-                   	   -- [epochs][band powers]
-    """
-
-    n_epochs = epochs.shape[2]
-
-    # call compute_band_powers for each epoch
-    for epoch in range(n_epochs):
-        # first epoch initialze matrix 
-        if epoch == 0:
-            feat           = compute_band_powers(epochs[:, :, epoch], freq).T
-            feature_matrix = np.zeros((n_epochs, feat.shape[0])) 
-
-        feature_matrix[epoch, :] = compute_band_powers(
-                epochs[:, :, epoch], freq).T
-
-    return feature_matrix 
-
-
-def train_classifier(feature_m0, feature_m1, feature_m2, feature_m3, 
-                        algorithm = 'RandomForestClassifier'):
-	""" Trains a random forest classifier from 5 different feature matrices
+	""" Title: BCI Workshop Auxiliary Tools
+	Author: Cassani
+	Date: May 08 2015
+	Availability: https://github.com/NeuroTechX/bci-workshop """
+	""" Computes band powers for multiple epochs
 
 	Arguments:
 
-		feature_m0 -- feature matrix [epochs][band powers]
-		feature_m1 -- feature matrix [epochs][band powers]
-		feature_m2 -- feature matrix [epochs][band powers]
-		feature_m3 -- feature matrix [epochs][band powers]
-		feature_m4 -- feature matrix [epochs][band powers]
-		algorithm  -- (str) type of algorithm to use
+	epochs         -- 3D array of epochs [epoch_samples, channels, epochs]
+	freq           -- (float)sample rate
 
-	Returns: 
- 
-		clf        -- (sklean object) RandomForestClassifer
-		mu_ft	   -- array that holds normalization mean
-		std_ft	   -- array that holds the nomralisation standard deviation
-		score 	   -- accuracy of model 
-	
+	Returns:
+
+	feature_matrix -- matrix of epochs and corresponding features
+	-- [epochs][band powers]
+	"""
+
+	n_epochs = epochs.shape[2]
+
+	# call compute_band_powers for each epoch
+	for epoch in range(n_epochs):
+		# first epoch initialze matrix
+		if epoch == 0:
+			feat           = compute_band_powers(epochs[:, :, epoch], freq).T
+			feature_matrix = np.zeros((n_epochs, feat.shape[0]))
+
+			feature_matrix[epoch, :] = compute_band_powers(
+			epochs[:, :, epoch], freq).T
+
+			return feature_matrix
+
+
+def train_classifier(feature_m0, feature_m1, feature_m2, feature_m3,
+algorithm = 'RandomForestClassifier'):
+	""" Trains a random forest classifier from 4 different feature matrices
+
+	Arguments:
+
+	feature_m0 -- feature matrix [epochs][band powers]
+	feature_m1 -- feature matrix [epochs][band powers]
+	feature_m2 -- feature matrix [epochs][band powers]
+	feature_m3 -- feature matrix [epochs][band powers]
+	algorithm  -- (str) type of algorithm to use
+
+	Returns:
+
+	clf        -- (sklean object) RandomForestClassifer
+	mu_ft	   -- array that holds normalization mean
+	std_ft	   -- array that holds the nomralisation standard deviation
+	score 	   -- accuracy of model
+
 	"""
 
 	# Create vector Y (class labels)
-    class0 = np.zeros((feature_matrix_0.shape[0], 1))
-    class1 = np.ones((feature_matrix_1.shape[0], 1))
-    class2 = np.full((feature_matrix_2.shape[0], 1), fill_value = 2)
-    class3 = np.full((feature_matrix_3.shape[0], 1), fill_value = 3)
+	class0 = np.zeros((feature_m0.shape[0], 1))
+	class1 = np.ones((feature_m1.shape[0], 1))
+	class2 = np.full((feature_m2.shape[0], 1), fill_value = 2)
+	class3 = np.full((feature_m3.shape[0], 1), fill_value = 3)
 
 
-    # Concatenate class labels
-    y = np.concatenate((class0, class1, class2, class3, class4), axis=0)
-   
-    # Concatenate feature matrices
-    features_all = np.concatenate((feature_matrix_0, 
-    							   feature_matrix_1, 
-    							   feature_matrix_2, 
-    							   feature_matrix_3, 
-    							   feature_matrix_4),
-                                   axis=0)
+	# Concatenate class labels
+	y = np.concatenate((class0, class1, class2, class3), axis=0)
 
-    # Normalize features columnwise
-    mu_ft  = np.mean(features_all, axis=0)
-    std_ft = np.std(features_all, axis=0)
+	# Concatenate feature matrices
+	features_all = np.concatenate((feature_m0,
+	feature_m1,
+	feature_m2,
+	feature_m3),
+	axis=0)
+
+	# Normalize features columnwise
+	mu_ft  = np.mean(features_all, axis=0)
+	std_ft = np.std(features_all, axis=0)
 	X      = (features_all - mu_ft) / std_ft
 
-    # Train RandomForestclassifier using default parameters
-    clf = RandomForestClassifier()
-    clf.fit(X,y)
-    score = clf.score(X, y.ravel())
+	# Train RandomForestclassifier using default parameters
+	clf = RandomForestClassifier()
+	clf.fit(X,y)
+	score = clf.score(X, y.ravel())
 
-    return clf, mu_ft, std_ft, score
+	return clf, mu_ft, std_ft, score
 
 
 def test_classifier(clf, feat_vector, mu_ft, std_ft):
-    """ Title: BCI Workshop Auxiliary Tools
-        Author: Cassani
-        Date: May 08 2015
-        Availability: https://github.com/NeuroTechX/bci-workshop """
-    """ Test the classifier on new data points.
+	""" Title: BCI Workshop Auxiliary Tools
+	Author: Cassani
+	Date: May 08 2015
+	Availability: https://github.com/NeuroTechX/bci-workshop """
+	""" Test the classifier on new data points.
 
-    Arguments:
-        clf         -- (sklean object) RandomForestClassifer
-		feat_vector -- array of features [samples][features]
-		mu_ft	    -- array that holds normalization mean
-		std_ft	    -- array that holds the normalization standard deviation
+	Arguments:
+	clf         -- (sklean object) RandomForestClassifer
+	feat_vector -- array of features [samples][features]
+	mu_ft	    -- array that holds normalization mean
+	std_ft	    -- array that holds the normalization standard deviation
 
-    Returns:
-        y_hat       -- decision of the classifier on data points
-    """
+	Returns:
+	y_hat       -- decision of the classifier on data points
+	"""
 
-    # Normalize feature_vector
-    x     = (feat_vector - mu_ft) / std_ft
-    y_hat = clf.predict(x)
+	# Normalize feature_vector
+	x     = (feat_vector - mu_ft) / std_ft
+	y_hat = clf.predict(x)
 
-    return y_hat
-
+	return y_hat
 
 class DataPlotter():
-    """ Title: BCI Workshop Auxiliary Tools
-        Author: Cassani
-        Date: May 08 2015
-        Availability: https://github.com/NeuroTechX/bci-workshop """
     """
     Class for creating and updating a line plot.
     """
@@ -353,19 +354,19 @@ class DataPlotter():
         """Initialize the figure."""
 
         self.nbPoints = nbPoints
-        self.chNames  = chNames
-        self.nbCh     = len(self.chNames)
-		
-		self.fs       = 1 if fs is None else fs
+        self.chNames = chNames
+        self.nbCh = len(self.chNames)
+
+        self.fs = 1 if fs is None else fs
         self.figTitle = '' if title is None else title
 
-        data   = np.empty((self.nbPoints, 1))*np.nan
+        data = np.empty((self.nbPoints, 1))*np.nan
         self.t = np.arange(data.shape[0])/float(self.fs)
 
         # Create offset parameters for plotting multiple signals
         self.yAxisRange = 100
-        self.chRange    = self.yAxisRange/float(self.nbCh)
-        self.offsets    = np.round((np.arange(self.nbCh)+0.5)*(self.chRange))
+        self.chRange = self.yAxisRange/float(self.nbCh)
+        self.offsets = np.round((np.arange(self.nbCh)+0.5)*(self.chRange))
 
         # Create the figure and axis
         plt.ion()
@@ -390,9 +391,8 @@ class DataPlotter():
     def update_plot(self, data):
         """ Update the plot """
 
-        data     = data - np.mean(data, axis=0)
+        data = data - np.mean(data, axis=0)
         std_data = np.std(data, axis=0)
-
         std_data[np.where(std_data == 0)] = 1
         data = data/std_data*self.chRange/5.0
 
